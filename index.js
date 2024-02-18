7f444a40ffe843624e9378dc700da21d7d3f651d
Hafeez200 <100261417+Hafeez200@users.noreply.github.com>
2024-02-18 11:09:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Task 1
+// function display_mess(){
+//     let name=prompt("Enter a number");
+//     alert("Welcome "+name);
+// }
+// display_mess();
+// Task 2
+// const qoutes=[
+//     "Give respect and Get Respect.",
+//     "success is the key to happiness.Happiness is the key to success.",
+//    "In the middle of difficulty lies opportunity." ,
+//    "Believe you can and you are Halfway there.",
+//    "The best way to predict the future is tocreate it. "
+// ];
+// function genteQoute(){
+//     const rndmind =Math.floor(Math.random()*qoutes.length);
+//     return qoutes[rndmind];
+// };
+// console.log(genteQoute());
+// Task 3
+// function randomNum(){
+//     return Math.floor(Math.random()*100)+1;
+// };
+// console.log(randomNum());
